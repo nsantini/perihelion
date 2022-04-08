9503dd61a5b0fca8bc2d4e498d3ebbb83a6f3d86
Nico <santini.nicolas@icloud.com>
2022-04-08 13:28:15 +1200

    load thread summary on feed and link to threads

--- pages/feed/[hops].js
+++ pages/feed/[hops].js
@@ -1,9 +1,8 @@
 import { useRouter } from "next/router";
-import { Stack } from "@chakra-ui/react";
+import { Stack, Center, Link, useColorModeValue } from "@chakra-ui/react";
 import MessageCard from "../../components/molecules/message";
 import MessageForm from "../../components/molecules/messageForm";
 import Container from "../../components/atoms/container";
-import Thread from "../../components/organisms/thread";
 import useFeed from "../../hooks/feed";
 
 export default function Feed() {
@@ -23,7 +22,20 @@ export default function Feed() {
 
       {feed &&
         feed.map &&
-        feed.map((thread, idx) => <Thread thread={thread} key={idx} />)}
+        feed.map((thread, index) => (
+          <Container key={index}>
+            <MessageCard {...thread.messages[0]} />
+            <Center>
+              <Link
+                color={useColorModeValue("frost.700", "frost.500")}
+                mt="2"
+                href={`/thread/${encodeURIComponent(thread.messages[0].key)}`}
+              >
+                {thread.replyCount} replies
+              </Link>
+            </Center>
+          </Container>
+        ))}
     </Stack>
   );
 }
