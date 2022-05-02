ebce71c844e273c57b543e9d9ad8a311e8feb549
Nico <santini.nicolas@icloud.com>
2022-05-03 07:57:47 +1200

    Properly inject custom ssb config

--- ssb/server.js
+++ ssb/server.js
@@ -1,5 +1,5 @@
 const SecretStack = require("secret-stack");
-const ssbConfig = require("ssb-config");
+const Config = require("ssb-config/inject");
 const caps = require("ssb-caps");
 
 module.exports = () => {
@@ -14,43 +14,65 @@ module.exports = () => {
     .use(require("ssb-db2"))
     .use(require("ssb-db2/compat"))
     .use(require("ssb-db2/about-self")) // include index
-    // // Replication
+    // Replication
     .use(require("ssb-ebt")) // needs: db2/compat
     .use(require("ssb-friends")) // needs: db2
     .use(require("ssb-replicate"))
     .use(require("ssb-replication-scheduler")) // needs: friends, ebt
-    // // Connections
+    // Connections
     .use(require("ssb-conn"))
     .use(require("ssb-lan"))
     .use(require("ssb-room-client")) // needs: conn
     .use(require("ssb-http-auth-client")) // needs: conn
     .use(require("ssb-http-invite-client"))
     .use(require("ssb-invite-client")) // needs: db2, conn
-    // // Queries
+    // Queries
     .use(require("ssb-threads")) // needs: db, db2, friends
     // .use(require('ssb-search2')) // needs: db2
     .use(require("ssb-blobs"));
 
-  // global._ssbServer = ssb(ssbConfig);
-  global._ssbServer = ssb({
-    ...ssbConfig,
+  const ssbConfig = Config("ssb", {
     db2: {
       automigrate: true,
       dangerouslyKillFlumeWhenMigrated: true,
     },
     replicate: {
       legacy: true,
-      fallback: false,
+      fallback: true,
     },
     connections: {
       incoming: {
+        net: [{ scope: "private", transform: "shs", port: 26831 }],
+        channel: [{ scope: "device", transform: "noauth" }],
+        bluetooth: [{ scope: "public", transform: "shs" }],
         tunnel: [{ scope: "public", transform: "shs" }],
       },
       outgoing: {
         net: [{ transform: "shs" }],
+        ws: [{ transform: "shs" }],
+        bluetooth: [{ scope: "public", transform: "shs" }],
         tunnel: [{ transform: "shs" }],
       },
     },
   });
+  global._ssbServer = ssb(ssbConfig);
+
+  // Connect to pubs
+  const connectToPubs = () => {
+    global._ssbServer.conn.dbPeers().forEach(([addr, data]) => {
+      if (data.type === "pub" && data.autoconnect) {
+        console.log(`Connecting to Pub ${data.name}: ${data.key}`);
+        global._ssbServer.conn.connect(addr, (err, connData) => {
+          if (err) console.error(err);
+          else {
+            console.log("Connected", connData.id);
+          }
+        });
+      }
+    });
+  };
+
+  setTimeout(connectToPubs, 5000);
+
   return global._ssbServer;
 };
