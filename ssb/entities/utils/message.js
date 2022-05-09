7ba0230b3dfbad663cbcdcf8389f5daba2d437cd
Nico <santini.nicolas@icloud.com>
2022-05-09 16:02:17 +1200

    support for Blog Posts

--- ssb/entities/utils/message.js
+++ ssb/entities/utils/message.js
@@ -26,12 +26,19 @@ module.exports = async (ssb, msg) => {
       })
     );
   }
+  let text = msg.value.content.text;
+  if (msg.value.content.type === "blog") {
+    const blog = await blob.getBlob(ssb, msg.value.content.blog);
+    text = `# ${msg.value.content.title}\n\n> ${
+      msg.value.content.summary
+    }\n\n---\n${Buffer.from(blog || "", "base64").toString("ascii")})`;
+  }
   const voters = await votes.getVotes(ssb, msg.key);
   return {
     msgId: msg.key,
     author: msg.value.author,
     timestamp: msg.value.timestamp,
-    text: msg.value.content.text,
+    text,
     recps: msg.value.content.recps,
     blobs,
     voters,
