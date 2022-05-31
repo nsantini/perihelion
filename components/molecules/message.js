ae2b1525d8264ea078fc78ee3ae42785d8a476fc
Nico <santini.nicolas@icloud.com>
2022-05-31 14:02:05 +1200

    showing voters profiles

--- components/molecules/message.js
+++ components/molecules/message.js
@@ -1,7 +1,39 @@
-import { IconButton, Flex, Text, useColorModeValue } from "@chakra-ui/react";
+import {
+  IconButton,
+  Flex,
+  Text,
+  Link,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverBody,
+} from "@chakra-ui/react";
 import { StarIcon } from "@chakra-ui/icons";
 import Content from "../atoms/content";
 import Author from "./author";
+import Avatar from "../atoms/avatar";
+import useProfile from "../../hooks/profile";
+
+function Voter({ profileId }) {
+  const { profile, isLoading, isError } = useProfile(profileId);
+  if (isError) return <div>Failed to load user details</div>;
+  if (isLoading) return <div>Loading...</div>;
+  return (
+    <Flex direction={"row"} textAlign={"left"} p={1}>
+      <Avatar image={profile.imageBlob} size={"sm"} />
+      <Link
+        ml={2}
+        pt={1}
+        fontFamily={"Work Sans"}
+        fontWeight={"bold"}
+        fontSize={16}
+        href={`/profile/${encodeURIComponent(profile.id)}`}
+      >
+        {profile.name}
+      </Link>
+    </Flex>
+  );
+}
 
 export default function MessageCard(props) {
   const { author, timestamp, text, blobs, index, voters, msgId } = props;
@@ -38,9 +70,21 @@ export default function MessageCard(props) {
       <Author author={author} timestamp={timestamp} />
       <Content text={text} blobs={blobs} />
       <Flex justifyContent={"right"}>
-        <Text mt={2} mr={1}>
-          {voters.length}
-        </Text>
+        <Popover trigger="hover">
+          <PopoverTrigger>
+            <Text mt={2} mr={1}>
+              {voters.length}
+            </Text>
+          </PopoverTrigger>
+          <PopoverContent>
+            <PopoverBody>
+              {voters.map((voter) => (
+                <Voter profileId={voter} />
+              ))}
+            </PopoverBody>
+          </PopoverContent>
+        </Popover>
+
         <IconButton onClick={vote} icon={<StarIcon />} />
       </Flex>
     </Flex>
